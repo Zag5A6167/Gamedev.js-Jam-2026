68044b2d19a52c1d3cd019f68dcf2de22455b521
z <za1a2ew44zz_999@hotmail.com>
2026-04-13 23:24:58 +0700

    add main player

---
+++ player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta: float) -> void:
+	
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction != Vector2.ZERO:
+		
+		velocity = direction * SPEED
+	else:
+		
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+
+
+	move_and_slide()
