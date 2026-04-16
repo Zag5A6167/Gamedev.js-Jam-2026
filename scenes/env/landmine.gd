a538947f6d07ef10c3e5af88e2b0a527e64d05bc
z <za1a2ew44zz_999@hotmail.com>
2026-04-17 05:23:33 +0700

    add: landmine

---
+++ scenes/env/landmine.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("Boom!")
+		
+		
+		if body.has_method("set_physics_process"):
+			body.set_physics_process(false) 
+		
+	
+		var tween = create_tween().set_loops()
+		
+		var spin_speed = 0.2
+		var rotation_amount = PI * 2 
+		
+
+		tween.tween_property(body, "rotation", body.rotation + rotation_amount, spin_speed) \
+			.set_trans(Tween.TRANS_LINEAR) 
+
+		
+		var fly_tween = create_tween()
+		fly_tween.tween_property(body, "position:y", body.position.y - 100000, 10.0)
