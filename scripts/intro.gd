203a6e5011cbe66108b85ad24bc11d74223f206e
TImessi <toto22767@gmail.com>
2026-04-22 22:51:27 +0700

    add new fixed

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,31 +1,49 @@
 extends VideoStreamPlayer
 
-@onready var color_rect = $ColorRect
-@onready var logo_1 = $logo1
-@onready var logo_2 = $logo2
+@onready var logo1 = $logo1
+@onready var logo2 = $logo2
 @onready var audio_player = $AudioStreamPlayer
 
 func _ready() -> void:
-	# 1. เตรียมความพร้อม ซ่อนโลโก้ก่อน
-	logo_1.hide()
-	logo_2.hide()
+	logo1.modulate.a = 0
+	logo2.modulate.a = 0
 	
-	# 2. เชื่อมต่อ Signal เมื่อวิดีโอเล่นจบ
-	finished.connect(_on_video_finished)
+	# เชื่อมต่อสัญญาณเมื่อวิดีโอจบ
+	self.finished.connect(_on_video_finished)
 	
-	# 3. เริ่มเล่นวิดีโอ Intro
+	# เริ่มเล่นวิดีโอทันที
 	play()
 
 func _input(event: InputEvent) -> void:
-	# ระบบ Skip: ถ้ากดปุ่มใดๆ หรือคลิกเมาส์ ให้ข้ามไปหน้าเมนูเลย
+	# ระบบ Skip: ถ้ากดปุ่มใดๆ หรือคลิกเมาส์ ให้ข้ามไปหน้าเมนูทันที
 	if event.is_pressed():
 		_transition_to_menu()
 
 func _on_video_finished() -> void:
-	# เมื่อวิดีโอจบ สามารถเลือกว่าจะแสดงโลโก้ต่อ หรือไปหน้าเมนูเลย
-	# ในที่นี้คือสั่งให้ไปหน้าเมนูทันที
-	_transition_to_menu()
+	var tween = create_tween()
+	
+	# แสดง Logo 1
+	tween.tween_callback(func(): audio_player.pitch_scale = 1.0)
+	tween.tween_callback(audio_player.play)
+	tween.tween_property(logo1, "modulate:a", 1.0, 1.0)
+	tween.tween_interval(1.5)
+	tween.tween_property(logo1, "modulate:a", 0.0, 1.0)
+	
+	# แสดง Logo 2
+	tween.tween_callback(func(): audio_player.pitch_scale = 1.5) 
+	tween.tween_callback(audio_player.play)
+	tween.tween_property(logo2, "modulate:a", 1.0, 1.0)
+	tween.tween_interval(1.5)
+	tween.tween_property(logo2, "modulate:a", 0.0, 1.0)
+	
+	# --- จุดที่หายไป: เมื่อ Tween ทั้งหมดทำงานจบ ให้ไปหน้าเมนู ---
+	tween.tween_callback(_transition_to_menu)
 
 func _transition_to_menu() -> void:
-	# เปลี่ยนฉากไปยังหน้า MenuScreen (ตรวจสอบ Path ให้ถูกต้อง)
-	get_tree().change_scene_to_file("res://scenes/menu_screen.tscn")
+	# ตรวจสอบ Path ให้ถูกต้องตามที่อยู่ใน FileSystem ของคุณ
+	var menu_path = "res://scenes/menu_screen.tscn"
+	
+	if ResourceLoader.exists(menu_path):
+		get_tree().change_scene_to_file(menu_path)
+	else:
+		print("Error: ไม่พบไฟล์หน้าเมนูที่ ", menu_path)
