eb6aadd3dc52f385dc6e79116125cef0ba422c53
z <za1a2ew44zz_999@hotmail.com>
2026-04-22 22:46:25 +0700

    add

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,31 +1,48 @@
 extends VideoStreamPlayer
 
-@onready var color_rect = $ColorRect
 @onready var logo_1 = $logo1
 @onready var logo_2 = $logo2
 @onready var audio_player = $AudioStreamPlayer
 
 func _ready() -> void:
-	# 1. เตรียมความพร้อม ซ่อนโลโก้ก่อน
-	logo_1.hide()
-	logo_2.hide()
+
+	logo_1.modulate.a = 0
+	logo_2.modulate.a = 0
+	logo_1.show()
+	logo_2.show()
 	
-	# 2. เชื่อมต่อ Signal เมื่อวิดีโอเล่นจบ
 	finished.connect(_on_video_finished)
-	
-	# 3. เริ่มเล่นวิดีโอ Intro
 	play()
 
-func _input(event: InputEvent) -> void:
-	# ระบบ Skip: ถ้ากดปุ่มใดๆ หรือคลิกเมาส์ ให้ข้ามไปหน้าเมนูเลย
-	if event.is_pressed():
-		_transition_to_menu()
 
 func _on_video_finished() -> void:
-	# เมื่อวิดีโอจบ สามารถเลือกว่าจะแสดงโลโก้ต่อ หรือไปหน้าเมนูเลย
-	# ในที่นี้คือสั่งให้ไปหน้าเมนูทันที
+
+	
+
+	var tween1 = create_tween()
+	
+	tween1.tween_property(logo_1, "modulate:a", 1.0, 1.0)
+	
+	tween1.tween_property(logo_1, "modulate:a", 0.0, 1.0)
+	
+
+	await tween1.finished
+
+	var tween2 = create_tween()
+	
+	tween2.tween_property(logo_2, "modulate:a", 1.0, 1.0)
+
+	tween2.tween_interval(1.5)
+
+	tween2.tween_property(logo_2, "modulate:a", 0.0, 1.0)
+	
+	
+	await tween2.finished
+	
+
 	_transition_to_menu()
 
 func _transition_to_menu() -> void:
-	# เปลี่ยนฉากไปยังหน้า MenuScreen (ตรวจสอบ Path ให้ถูกต้อง)
-	get_tree().change_scene_to_file("res://scenes/menu_screen.tscn")
+
+	
+		get_tree().change_scene_to_file("res://scenes/menu_screen.tscn")
